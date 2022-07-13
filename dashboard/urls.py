783aa6675ea7a611dd0e2fab0c7c186e795b4017
bvrti <74336093+bvrti@users.noreply.github.com>
2022-07-13 10:12:50 +0200

    different images for multiple areas

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,7 +4,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('smt/', views.indexSmt, name = 'indexSmt'),
+    path('cba/', views.indexCba, name = 'indexCba'),
+    path('fa/', views.indexFa, name = 'indexFa'),
     path('upload/', views.uploadImg, name = 'uploadImg'),
+    path('uploadSmt/', views.uploadImgSmt, name = 'uploadImgSmt'),
+    path('uploadCba/', views.uploadImgCba, name = 'uploadImgCba'),
+    path('uploadFa/', views.uploadImgFa, name = 'uploadImgFa'),
     path('logout/', views.logout, name = 'logout'),
     path('login/', include("django.contrib.auth.urls")),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -10,6 +10,18 @@ def index(request):
     template = loader.get_template('dashboard/index.html')
     return HttpResponse(template.render({}, request))
 
+def indexSmt(request):
+    template = loader.get_template('dashboard/index_smt.html')
+    return HttpResponse(template.render({}, request))
+
+def indexCba(request):
+    template = loader.get_template('dashboard/index_cba.html')
+    return HttpResponse(template.render({}, request))
+
+def indexFa(request):
+    template = loader.get_template('dashboard/index_fa.html')
+    return HttpResponse(template.render({}, request))
+
 @login_required(login_url='/login/')
 def uploadImg(request):
     if request.method == 'POST':
@@ -38,6 +50,94 @@ def uploadImg(request):
         return render(request, 'dashboard/upload.html', {'file_url': file_url})
     template = loader.get_template('dashboard/upload.html')
     return HttpResponse(template.render({}, request))
+
+@login_required(login_url='/login/')
+def uploadImgSmt(request):
+    if request.method == 'POST':
+        if request.FILES.get('upload1'):
+            upload = request.FILES.get('upload1')
+            name = 'smt/1.jpg'
+        elif request.FILES.get('upload2'):
+            upload = request.FILES.get('upload2')
+            name = 'smt/2.jpg'
+        elif request.FILES.get('upload3'):
+            upload = request.FILES.get('upload3')
+            name = 'smt/3.jpg'
+        elif request.FILES.get('upload4'):
+            upload = request.FILES.get('upload4')
+            name = 'smt/4.jpg'
+        elif request.FILES.get('upload5'):
+            upload = request.FILES.get('upload5')
+            name = 'smt/5.jpg'
+        elif request.FILES.get('upload6'):
+            upload = request.FILES.get('upload6')
+            name = 'smt/6.jpg'
+        fss = FileSystemStorage()
+        file_rm = fss.delete(name)
+        file = fss.save(name, upload)
+        file_url = fss.url(file)
+        return render(request, 'dashboard/upload_smt.html', {'file_url': file_url})
+    template = loader.get_template('dashboard/upload_smt.html')
+    return HttpResponse(template.render({}, request))
+
+@login_required(login_url='/login/')
+def uploadImgCba(request):
+    if request.method == 'POST':
+        if request.FILES.get('upload1'):
+            upload = request.FILES.get('upload1')
+            name = 'cba/1.jpg'
+        elif request.FILES.get('upload2'):
+            upload = request.FILES.get('upload2')
+            name = 'cba/2.jpg'
+        elif request.FILES.get('upload3'):
+            upload = request.FILES.get('upload3')
+            name = 'cba/3.jpg'
+        elif request.FILES.get('upload4'):
+            upload = request.FILES.get('upload4')
+            name = 'cba/4.jpg'
+        elif request.FILES.get('upload5'):
+            upload = request.FILES.get('upload5')
+            name = 'cba/5.jpg'
+        elif request.FILES.get('upload6'):
+            upload = request.FILES.get('upload6')
+            name = 'cba/6.jpg'
+        fss = FileSystemStorage()
+        file_rm = fss.delete(name)
+        file = fss.save(name, upload)
+        file_url = fss.url(file)
+        return render(request, 'dashboard/upload_cba.html', {'file_url': file_url})
+    template = loader.get_template('dashboard/upload_cba.html')
+    return HttpResponse(template.render({}, request))
+
+@login_required(login_url='/login/')
+def uploadImgFa(request):
+    if request.method == 'POST':
+        if request.FILES.get('upload1'):
+            upload = request.FILES.get('upload1')
+            name = 'fa/1.jpg'
+        elif request.FILES.get('upload2'):
+            upload = request.FILES.get('upload2')
+            name = 'fa/2.jpg'
+        elif request.FILES.get('upload3'):
+            upload = request.FILES.get('upload3')
+            name = 'fa/3.jpg'
+        elif request.FILES.get('upload4'):
+            upload = request.FILES.get('upload4')
+            name = 'fa/4.jpg'
+        elif request.FILES.get('upload5'):
+            upload = request.FILES.get('upload5')
+            name = 'fa/5.jpg'
+        elif request.FILES.get('upload6'):
+            upload = request.FILES.get('upload6')
+            name = 'fa/6.jpg'
+        fss = FileSystemStorage()
+        file_rm = fss.delete(name)
+        file = fss.save(name, upload)
+        file_url = fss.url(file)
+        return render(request, 'dashboard/upload_fa.html', {'file_url': file_url})
+    template = loader.get_template('dashboard/upload_fa.html')
+    return HttpResponse(template.render({}, request))
+
 
 def logout(request):
     return redirect('../../dashboard/')
